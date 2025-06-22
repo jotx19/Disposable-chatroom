3239fb670acf0b327c2a04efefa8cb6a2ea22fe7
Prabjot Singh <prabjot24x7@gmail.com>
2025-06-22 13:03:24 -0400

    debugv1

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,5 @@
 import express from "express"
-import passport from "passport";
 import { signup, login, logout, updateProfile, checkAuth } from "../controllers/auth.controller.js";
-import { googleAuth } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { configurePassport } from "../lib/passport.js";
 
@@ -17,8 +15,5 @@ router.post("/logout",logout);
 router.put("/update-profile", protectRoute, updateProfile);
 
 router.get("/check", protectRoute, checkAuth);
-
-router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-router.get("/auth/google/callback", passport.authenticate("google", { session: false }),googleAuth);
 
 export default router;
